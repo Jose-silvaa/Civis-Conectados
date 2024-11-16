1c360c658a6206e67d0f5c4b26bbff144d35aed1
Jose-silvaa <jvitorfacanha@gmail.com>
2024-11-15 22:45:55 -0300

    Report page

---
+++ src/app/account/layout.js
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/SideBar";
+
+export default function AccountLayout({
+    children, 
+  }) {
+    return (
+      <section className="flex">
+       
+        <nav>
+          <Sidebar/>
+        </nav>
+   
+        {children}
+      </section>
+    )
+  }
