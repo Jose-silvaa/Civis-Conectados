63a686f3656d8ac8b91e08bad2bd5d3a25e887bc
Jose-silvaa <jvitorfacanha@gmail.com>
2024-12-09 21:23:01 -0300

    config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+import path from 'path'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  webpack : (config) =>{
+    config.resolve.alias['@'] = path.resolve('src');
+    return config;
+  },
     images: {
       remotePatterns: [
         {
